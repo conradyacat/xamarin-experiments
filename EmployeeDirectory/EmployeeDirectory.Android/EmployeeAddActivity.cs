f9aa0e8ea0b564f1af1d17059cd9e967bf9e8342
cyacat <conrad.yacat@gmail.com>
2016-07-13 18:37:01 +0800

    initial implementation of photo capturing

--- EmployeeDirectory/EmployeeDirectory.Android/EmployeeAddActivity.cs
+++ EmployeeDirectory/EmployeeDirectory.Android/EmployeeAddActivity.cs
@@ -1,11 +1,17 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Net;
 using Android.OS;
+using Android.Provider;
 using Android.Support.Design.Widget;
 using Android.Support.V4.App;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using EmployeeDirectory.Android.Database;
+using EmployeeDirectory.Android.Util;
+using Java.IO;
 using CompatV7 = Android.Support.V7;
 
 namespace EmployeeDirectory.Android
@@ -14,6 +20,9 @@ namespace EmployeeDirectory.Android
 	[MetaData("android.support.PARENT_ACTIVITY", Value = "EmployeeDirectory.Android.MainActivity")]
 	public class EmployeeAddActivity : AppCompatActivity
 	{
+		private ImageButton _imageButton;
+		private File _file;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -23,6 +32,15 @@ namespace EmployeeDirectory.Android
 			var topToolbar = FindViewById<CompatV7.Widget.Toolbar>(Resource.Id.topToolbar);
 			SetSupportActionBar(topToolbar);
 			SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+			_imageButton = FindViewById<ImageButton>(Resource.Id.photo);
+			_imageButton.Click += (sender, e) =>
+			{
+				_file = new File(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), System.Guid.NewGuid().ToString());
+				var intent = new Intent(MediaStore.ActionImageCapture);
+				intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(_file));
+				StartActivityForResult(intent, 0);
+			};
 
 			var saveButton = FindViewById<Button>(Resource.Id.saveButton);
 			saveButton.Click += (sender, e) =>
@@ -100,6 +118,15 @@ namespace EmployeeDirectory.Android
 				default:
 					return base.OnOptionsItemSelected(item);
 			}
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+
+			int height = Resources.DisplayMetrics.HeightPixels;
+			int width = _imageButton.Height;
+			Bitmap bitmap = _file.Path.LoadAndResizeBitmap(height, width);
 		}
 	}
 }
--- EmployeeDirectory/EmployeeDirectory.Android/Resources/Resource.Designer.cs
+++ EmployeeDirectory/EmployeeDirectory.Android/Resources/Resource.Designer.cs
@@ -1828,8 +1828,8 @@ namespace EmployeeDirectory.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06007f
-			public const int action0 = 2131099775;
+			// aapt resource value: 0x7f060080
+			public const int action0 = 2131099776;
 			
 			// aapt resource value: 0x7f06005a
 			public const int action_bar = 2131099738;
@@ -1855,8 +1855,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06005b
 			public const int action_context_bar = 2131099739;
 			
-			// aapt resource value: 0x7f060083
-			public const int action_divider = 2131099779;
+			// aapt resource value: 0x7f060084
+			public const int action_divider = 2131099780;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -1891,8 +1891,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f060044
 			public const int buttonPanel = 2131099716;
 			
-			// aapt resource value: 0x7f060080
-			public const int cancel_action = 2131099776;
+			// aapt resource value: 0x7f060081
+			public const int cancel_action = 2131099777;
 			
 			// aapt resource value: 0x7f06002b
 			public const int center = 2131099691;
@@ -1906,8 +1906,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f060052
 			public const int checkbox = 2131099730;
 			
-			// aapt resource value: 0x7f060086
-			public const int chronometer = 2131099782;
+			// aapt resource value: 0x7f060087
+			public const int chronometer = 2131099783;
 			
 			// aapt resource value: 0x7f060033
 			public const int clip_horizontal = 2131099699;
@@ -1954,8 +1954,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06005c
 			public const int edit_query = 2131099740;
 			
-			// aapt resource value: 0x7f060078
-			public const int email = 2131099768;
+			// aapt resource value: 0x7f060079
+			public const int email = 2131099769;
 			
 			// aapt resource value: 0x7f060072
 			public const int empAddLayout = 2131099762;
@@ -1963,8 +1963,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06001c
 			public const int end = 2131099676;
 			
-			// aapt resource value: 0x7f06008b
-			public const int end_padder = 2131099787;
+			// aapt resource value: 0x7f06008c
+			public const int end_padder = 2131099788;
 			
 			// aapt resource value: 0x7f060023
 			public const int enterAlways = 2131099683;
@@ -1990,8 +1990,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06002e
 			public const int fill_vertical = 2131099694;
 			
-			// aapt resource value: 0x7f060074
-			public const int firstName = 2131099764;
+			// aapt resource value: 0x7f060075
+			public const int firstName = 2131099765;
 			
 			// aapt resource value: 0x7f060038
 			public const int @fixed = 2131099704;
@@ -2011,23 +2011,23 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06003f
 			public const int image = 2131099711;
 			
-			// aapt resource value: 0x7f06008a
-			public const int info = 2131099786;
+			// aapt resource value: 0x7f06008b
+			public const int info = 2131099787;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
 			
-			// aapt resource value: 0x7f060075
-			public const int lastName = 2131099765;
+			// aapt resource value: 0x7f060076
+			public const int lastName = 2131099766;
 			
 			// aapt resource value: 0x7f06002f
 			public const int left = 2131099695;
 			
-			// aapt resource value: 0x7f060084
-			public const int line1 = 2131099780;
+			// aapt resource value: 0x7f060085
+			public const int line1 = 2131099781;
 			
-			// aapt resource value: 0x7f060088
-			public const int line3 = 2131099784;
+			// aapt resource value: 0x7f060089
+			public const int line3 = 2131099785;
 			
 			// aapt resource value: 0x7f06000b
 			public const int listMode = 2131099659;
@@ -2035,17 +2035,17 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f060041
 			public const int list_item = 2131099713;
 			
-			// aapt resource value: 0x7f06007e
-			public const int mainLayout = 2131099774;
+			// aapt resource value: 0x7f06007f
+			public const int mainLayout = 2131099775;
 			
-			// aapt resource value: 0x7f060082
-			public const int media_actions = 2131099778;
+			// aapt resource value: 0x7f060083
+			public const int media_actions = 2131099779;
+			
+			// aapt resource value: 0x7f06008e
+			public const int menu_add_emp = 2131099790;
 			
 			// aapt resource value: 0x7f06008d
-			public const int menu_add_emp = 2131099789;
-			
-			// aapt resource value: 0x7f06008c
-			public const int menu_search = 2131099788;
+			public const int menu_search = 2131099789;
 			
 			// aapt resource value: 0x7f06001d
 			public const int middle = 2131099677;
@@ -2053,17 +2053,17 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f060037
 			public const int mini = 2131099703;
 			
-			// aapt resource value: 0x7f060077
-			public const int mobilePhone = 2131099767;
+			// aapt resource value: 0x7f060078
+			public const int mobilePhone = 2131099768;
 			
-			// aapt resource value: 0x7f06007c
-			public const int mobilePhoneNumber = 2131099772;
+			// aapt resource value: 0x7f06007d
+			public const int mobilePhoneNumber = 2131099773;
 			
 			// aapt resource value: 0x7f060016
 			public const int multiply = 2131099670;
 			
-			// aapt resource value: 0x7f06007a
-			public const int name = 2131099770;
+			// aapt resource value: 0x7f06007b
+			public const int name = 2131099771;
 			
 			// aapt resource value: 0x7f06006d
 			public const int navigation_header_container = 2131099757;
@@ -2077,17 +2077,20 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06000c
 			public const int normal = 2131099660;
 			
-			// aapt resource value: 0x7f060076
-			public const int officePhone = 2131099766;
+			// aapt resource value: 0x7f060077
+			public const int officePhone = 2131099767;
 			
-			// aapt resource value: 0x7f06007b
-			public const int officePhoneNumber = 2131099771;
+			// aapt resource value: 0x7f06007c
+			public const int officePhoneNumber = 2131099772;
 			
 			// aapt resource value: 0x7f060028
 			public const int parallax = 2131099688;
 			
 			// aapt resource value: 0x7f060046
 			public const int parentPanel = 2131099718;
+			
+			// aapt resource value: 0x7f060074
+			public const int photo = 2131099764;
 			
 			// aapt resource value: 0x7f060029
 			public const int pin = 2131099689;
@@ -2101,14 +2104,14 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f060054
 			public const int radio = 2131099732;
 			
-			// aapt resource value: 0x7f06007d
-			public const int refresher = 2131099773;
+			// aapt resource value: 0x7f06007e
+			public const int refresher = 2131099774;
 			
 			// aapt resource value: 0x7f060030
 			public const int right = 2131099696;
 			
-			// aapt resource value: 0x7f060079
-			public const int saveButton = 2131099769;
+			// aapt resource value: 0x7f06007a
+			public const int saveButton = 2131099770;
 			
 			// aapt resource value: 0x7f060017
 			public const int screen = 2131099671;
@@ -2200,8 +2203,8 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f060031
 			public const int start = 2131099697;
 			
-			// aapt resource value: 0x7f060081
-			public const int status_bar_latest_event_content = 2131099777;
+			// aapt resource value: 0x7f060082
+			public const int status_bar_latest_event_content = 2131099778;
 			
 			// aapt resource value: 0x7f060065
 			public const int submit_area = 2131099749;
@@ -2209,17 +2212,17 @@ namespace EmployeeDirectory.Android
 			// aapt resource value: 0x7f06000d
 			public const int tabMode = 2131099661;
 			
-			// aapt resource value: 0x7f060089
-			public const int text = 2131099785;
+			// aapt resource value: 0x7f06008a
+			public const int text = 2131099786;
 			
-			// aapt resource value: 0x7f060087
-			public const int text2 = 2131099783;
+			// aapt resource value: 0x7f060088
+			public const int text2 = 2131099784;
 			
 			// aapt resource value: 0x7f06004d
 			public const int textSpacerNoButtons = 2131099725;
 			
-			// aapt resource value: 0x7f060085
-			public const int time = 2131099781;
+			// aapt resource value: 0x7f060086
+			public const int time = 2131099782;
 			
 			// aapt resource value: 0x7f060043
 			public const int title = 2131099715;
